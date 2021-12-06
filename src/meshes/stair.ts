822f12aba3c2bc5a7a17c01a94b0c26672b241cf
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-12-06 17:49:27 +0100

    adding stairs from merged mesh stairs

--- src/meshes/stair.ts
+++ src/meshes/stair.ts
@@ -3,16 +3,38 @@ import { createColorMaterial } from "../materials/surfaceColor";
 
 export const buildStairs = (scene: Scene) => {
 
-    const stairPlane: Mesh = MeshBuilder.CreatePlane('stair1', {width: 100, height: 30, sideOrientation: Mesh.DOUBLESIDE}, scene); 
-    stairPlane.position = new Vector3(-149, 85, -239)
+    const stairPlane: Mesh = MeshBuilder.CreatePlane('stair1', {width: 100, height: 15, sideOrientation: Mesh.DOUBLESIDE}, scene); 
+    stairPlane.position =  new Vector3 (-149, 95, -240);
     stairPlane.material = createColorMaterial(scene).stairColor;
 
     const stairInstance: Mesh = MeshBuilder.CreatePlane('stair1', {width: 100, height: 30, sideOrientation: Mesh.DOUBLESIDE}, scene); 
     stairInstance.rotation = new Vector3(Math.PI/2, 0, 0);
-    stairInstance.position = new Vector3 (-149, 70, -225);
+    stairInstance.position = new Vector3 (-149, 88, -225);
 
     const stairs = Mesh.MergeMeshes([stairPlane, stairInstance]);
     
+
+
+    let stairsArray = [];
+    stairsArray.push([1, 0, -14, 30]);
+    stairsArray.push([1, 0, -28, 60]);
+    stairsArray.push([1, 0, -40, 90]);
+    stairsArray.push([1, 0, -54, 120]);
+   
+    let stepsArray = []
+
+    for (let i = 0; i<stairsArray.length; i++){
+        if (stairsArray[i][0] === 1){
+        stepsArray[i] = stairs.createInstance('instanceStairs' + i);
+        } else{
+            return;
+        }
+        stepsArray[i].position.x = stairsArray[i][1]
+        stepsArray[i].position.y = stairsArray[i][2]
+        stepsArray[i].position.z = stairsArray[i][3]
+
+        
+    }
     
 
 }
