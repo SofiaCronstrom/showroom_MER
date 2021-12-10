acd33c7d78ea8b296f627fa02655347e1a90ded7
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-12-10 12:11:00 +0100

    refoctoring for loop and if statement in ceiling.ts

--- src/meshes/ceiling.ts
+++ src/meshes/ceiling.ts
@@ -10,31 +10,26 @@ export const buildCeiling = (scene: Scene) => {
     plane.rotation = new Vector3(Math.PI/2.7, Math.PI/2, 0)
     plane.material = createColorMaterial(scene).wallColor;
 
-    const thinPlane = plane.createInstance('thinPlane');
-
     //Left ceiling piece
     const leftPlane: Mesh = MeshBuilder.CreatePlane("ceilingLeft", {height:600, width: 260, sideOrientation: Mesh.DOUBLESIDE}, scene); 
     leftPlane.position = new Vector3(274.95, 497.32, 0);
     leftPlane.rotation = new Vector3(Math.PI/2.7, -Math.PI/2, 0)
 
     //Array of positions
-    let ceilingPlace = [];
+    let ceilingPlace: any[] = [];
+    
     ceilingPlace.push([1, -274.95, 497.32, 520.8]);
     ceilingPlace.push([1, -274.95, 497.32, -520.8]);
     ceilingPlace.push([2, 274.95, 497.32, -520.8]);
     ceilingPlace.push([2, 274.95, 497.32, 520.8]);
    
    //placing ceiling pieces at positions
-    let ceilingPiece = []
-    for (let i=0; i<ceilingPlace.length; i++){
+    let ceilingPiece: any[] = [];
+    for (let i in ceilingPlace){
 
-        if (ceilingPlace[i][0] === 1){
-          ceilingPiece[i] = plane.createInstance('ceilingPiece' + i);
-        } else if(ceilingPlace[i][0] === 2) { 
-           ceilingPiece[i] = leftPlane.createInstance('ceilingPieceLeft' + i);
-        }
-        
-        ceilingPiece[i].position.x = ceilingPlace[i][1];
+        (ceilingPlace[i][0] === 1) ? ceilingPiece[i] = plane.createInstance('ceilingPiece' + i) :  ceilingPiece[i] = leftPlane.createInstance('ceilingPieceLeft' + i); 
+       
+         ceilingPiece[i].position.x = ceilingPlace[i][1];
         ceilingPiece[i].position.y = ceilingPlace[i][2];
         ceilingPiece[i].position.z = ceilingPlace[i][3];
     }
